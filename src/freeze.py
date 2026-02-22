94130914941c3340a9bfa7a02ca774027e1f0fde
preston-thomas <140858770+preston-thomas@users.noreply.github.com>
2026-02-22 13:45:08 -0500

    Refactors for Pages deployment

--- src/freeze.py
+++ src/freeze.py
@@ -1,19 +1,21 @@
 import os
 import shutil
+
 from flask_frozen import Freezer
+
 from src import create_app
 
 app = create_app()
 app.config['FREEZER_DESTINATION'] = '../docs'
+app.config['FREEZER_RELATIVE_URLS'] = True
 freezer = Freezer(app)
 
-# Construct the absolute path for cleanup
+# Construct the absolute path for cleanup.
 destination = os.path.abspath(os.path.join(app.root_path, app.config['FREEZER_DESTINATION']))
 
-# Clean up the build directory before freezing
+# Clean up the build directory before freezing.
 if os.path.exists(destination):
     shutil.rmtree(destination)
-
 
 if __name__ == '__main__':
     freezer.freeze()
